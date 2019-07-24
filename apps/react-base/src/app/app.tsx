a8ba93bfbaac4fa9e418383c832b7c65168aaef0
Jonathan C <jchiang@8thlight.com>
2019-07-24 11:41:32 -0500

    Add main layout component

--- apps/react-base/src/app/app.tsx
+++ apps/react-base/src/app/app.tsx
@@ -1,35 +1,11 @@
 import React from 'react';
 
 import './app.css';
+import { MainLayout } from './components/layouts/MainLayout';
 
 export const App = () => {
   return (
-    <div>
-      <header style={{ textAlign: 'center' }}>
-        <h1>Welcome to react-base!</h1>
-        <img
-          width="450"
-          src="https://raw.githubusercontent.com/nrwl/nx/master/nx-logo.png"
-        />
-      </header>
-      <p>
-        This is a React app built with <a href="https://nx.dev">Nx</a>.
-      </p>
-      <p>🔎 **Nx is a set of Angular CLI power-ups for modern development.**</p>
-      <h2>Quick Start & Documentation</h2>
-      <ul>
-        <li>
-          <a href="https://nx.dev/getting-started/what-is-nx">
-            30-minute video showing all Nx features
-          </a>
-        </li>
-        <li>
-          <a href="https://nx.dev/tutorial/01-create-application">
-            Interactive tutorial
-          </a>
-        </li>
-      </ul>
-    </div>
+    <MainLayout></MainLayout>
   );
 };
 
---
+++ apps/react-base/src/app/components/layouts/MainLayout.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+export const MainLayout = () => {
+  return (
+    <div style={{ display: 'flex', flexFlow: 'column', alignItems: 'center', height: '100vh', width: '100%' }}>
+      <Router>
+        <nav style={{ display: 'flex',  alignItems: 'center', justifyContent: 'space-between', padding: '10px', background: 'gray', width: '100%' }}>
+            <Link to="/">Home</Link>
+            <Link to="/about/">About</Link>
+            <Link to="/users/">Users</Link>
+        </nav>
+        <div style={{ display: 'flex', flexFlow: 'column', flex: '1', alignItems: 'center', justifyContent: 'center', width: '100%', background: 'lightgray' }}>
+          <Route path="/" exact component={Index} />
+          <Route path="/about/" component={About} />
+          <Route path="/users/" component={Users} />
+        </div>
+      </Router>
+    </div>
+  );
+}
