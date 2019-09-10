e595ca837744c020a54838b3450d2ef8b8f0ed41
Jonathan C <jchiang@8thlight.com>
2019-09-10 17:51:29 -0500

    WIP: Add yup schema for create post form

---
+++ apps/react-base/src/app/components/forms/create-post-form.validation.ts
@@ -0,0 +1,19 @@
+import * as Yup from 'yup';
+
+export const initialValues = { postContent: '' };
+
+export const formSchemaForCreatePost = Yup.object().shape({
+	postContent: Yup.string()
+		.min(3, 'Must be at least 3 characters.')
+		.max(240, 'Max length is 240 characters.')
+		.required('Input is required.'),
+	// email: Yup.string()
+	// 	.email('Invalid email address')
+	// 	.required('Email is required.'),
+	// phone: Yup.string()
+	// 	.matches(phoneRegExp, 'Phone number is not valid.'),
+	// title: Yup.string()
+	// 	.min(2, 'Must be at least 2 characters.')
+	// 	.max(3, 'Max length is 3 characters.')
+	// 	.required('Title is required.'),
+});
