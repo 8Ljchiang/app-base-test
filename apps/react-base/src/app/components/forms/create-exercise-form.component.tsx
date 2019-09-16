b5f00b2daa4513b10c30835e1ebe2c5268408733
Jonathan C <jchiang@8thlight.com>
2019-09-16 17:49:25 -0500

    WIP: Add create exercise form component

---
+++ apps/react-base/src/app/components/forms/create-exercise-form.component.tsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+export class CreateExerciseForm extends Component<any, any> {
+  constructor(props) {
+    super(props);
+
+    this.createExercise = this.createExercise.bind(this);
+  }
+
+  createExercise(info) {
+    console.log('create exercise', info);
+  }
+
+  render() {
+    return (
+      <Formik
+        initialValues={}
+        validationSchema={}
+        onSubmit={(values, actions) => {
+          this.createExercise(values);
+          actions.resetForm();
+        }}
+        render={({ errors, status, touched, isSubmitting }) => (
+          <Form className="form-container" autoComplete="off">
+            <label htmlFor="title">Title</label>
+            <Field component="text" id="title" name="title" className="form-field"/>
+            <ErrorMessage name="title" component="div" />
+
+            <label htmlFor="desc">Description</label>
+            <Field component="text" id="desc" name="desc" className="form-field"/>
+            <ErrorMessage name="desc" component="div" />
+
+            <label htmlFor="repo">Repo / Reference</label>
+            <Field component="text" id="repo" name="repo" className="form-field"/>
+            <ErrorMessage name="repo" component="div" />
+
+            <button className="form-submit-button" type="submit" disabled={isSubmitting}>Submit</button>
+          </Form>
+        )}
+      />
+    );
+  }
+}
